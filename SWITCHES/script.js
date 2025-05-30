b284e18165106c12c92642f6368c22eced9bcbf1
topmost003 <topmostdabest@gmail.com>
2025-05-30 08:43:40 +0100

    switch

---
+++ SWITCHES/script.js
@@ -0,0 +1,62 @@
+// // let score = prompt(`enter your score`)
+
+// switch(true){
+//  case score >= 80 :
+//     alert (`A`)
+//  break;
+
+//  case score >= 70 : 
+//  alert (`B`)
+//  break;
+
+//  case score >= 65 :  
+//  alert (`C`)
+//  break;
+//  case score >= 55 :
+//      alert (`D`)
+//  break;
+//  case score >= 40 : 
+//  alert (`E`)
+//  break;
+//  case score < 40 : 
+//  alert (`F`)
+//  break;
+//  default : 
+//  alert(`enter a valid number`)
+// break;
+
+// }
+
+
+
+function submit(){
+let score = document.getElementById(`input`).value
+
+switch(true){
+ case score >= 80 :
+    document.getElementById(`grade`).innerText = "A"
+ break;
+
+ case score >= 70 : 
+  document.getElementById(`grade`).innerText = "B"
+ break;
+
+ case score >= 65 :  
+  document.getElementById(`grade`).innerText = "C"
+ break;
+ case score >= 55 :
+     document.getElementById(`grade`).innerText = "D"
+ break;
+ case score >= 40 : 
+  document.getElementById(`grade`).innerText = "E"
+ break;
+ case score < 39 : 
+  document.getElementById(`grade`).innerText = "F"
+ break;
+ default : 
+ alert(`enter a valid number`)
+break;
+
+}
+
+}
